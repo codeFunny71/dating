df4bb6d40a48eb02b725e8e7c042b168209ea8dd
Marcus Absher <mabsher@mail.greenriver.edu>
2019-02-14 16:11:35 -0500

    added member and premiereMember  classes

---
+++ classes/premiereMember.php
@@ -0,0 +1,63 @@
+<?php
+
+class PremiereMember extends Member
+{
+    private $_inDoorInterests = [];
+    private $_outDoorInterests = [];
+
+    /**
+     * PremiereMember constructor.
+     * @param array $_inDoorInterests
+     * @param array $_outDoorInterests
+     * Member constructor.
+     * @param $_fName
+     * @param $_lName
+     * @param $_age
+     * @param $_gender
+     * @param $_phone
+     * @param $_email
+     * @param $_state
+     * @param $_seeking
+     * @param $_bio
+     */
+    public function __construct(array $_inDoorInterests, array $_outDoorInterests, $_fName, $_lName, $_age, $_gender, $_phone, $_email, $_state, $_seeking, $_bio)
+    {
+        parent::__construct($_fName, $_lName, $_age, $_gender, $_phone, $_email, $_state, $_seeking, $_bio);
+        $this->_inDoorInterests = $_inDoorInterests;
+        $this->_outDoorInterests = $_outDoorInterests;
+    }
+
+    /**
+     * @return array
+     */
+    public function getInDoorInterests()
+    {
+        return $this->_inDoorInterests;
+    }
+
+    /**
+     * @param array $inDoorInterests
+     */
+    public function setInDoorInterests($inDoorInterests)
+    {
+        $this->_inDoorInterests = $inDoorInterests;
+    }
+
+    /**
+     * @return array
+     */
+    public function getOutDoorInterests()
+    {
+        return $this->_outDoorInterests;
+    }
+
+    /**
+     * @param array $outDoorInterests
+     */
+    public function setOutDoorInterests($outDoorInterests)
+    {
+        $this->_outDoorInterests = $outDoorInterests;
+    }
+
+
+}
